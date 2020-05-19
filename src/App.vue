4e904f9e1f27d53dc6229821e7961556a6466fc2
mariepahuus <mari54u0@easv365.dk>
2020-05-19 12:13:13 +0200

    hej

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,57 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div id="navigation">
+        <router-link to="/">Home</router-link> 
+        <router-link to="/about">Guidelines</router-link>
+        <router-link to="/about">Covid-19</router-link>
+      </div>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+
+$color-primary: #AF182E;
+$color-secondary: #000000;
+$font-primary: 'Futura';
+$font-secondary: 'Open Sans Condensed', sans-serif,;
+
+@mixin flexCenter($direction) {
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: $direction;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+  display: flex;
+  align-items: flex-end;
+  padding-left: 100px;
+  height: 100px;
+  width: 100%;
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
+    font-weight: medium;
+    color: $color-secondary;
+    text-decoration: none;
+    font-family: $font-primary;
+    font-size: 22px;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: $color-primary;
     }
   }
+}
+
+#navigation {
+  width: 40%;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <div id="main">
+
+   </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
 </script>
+
+<style>
+  #main {
+  background-image: url('../assets/background_1.svg');
+  height: 800px;
+  background-repeat: no-repeat;
+}
+</style>
